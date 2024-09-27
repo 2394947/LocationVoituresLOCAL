9eca910a500863f6e112f900f88c0ef5f2155933
2394947 <Jennifer.m1999@outlook.fr>
2024-09-27 18:54:54 -0400

    Ajout des trigger corrigés.

--- src/SQL/BDA_TP2_Trigger.sql
+++ src/SQL/BDA_TP2_Trigger.sql
@@ -1,194 +1,27 @@
 USE locationVoitures;
-
-------------- Fonction pour obtenir le nombre de jours de la location ------------------------------------------------
-
 GO
-CREATE OR ALTER FUNCTION FCT_ObtenirNbJoursLocation(@locationId INT)
-RETURNS INT
+
+CREATE OR ALTER TRIGGER TR_UnSeulEnregistrementTableTarif
+ON tarifLocation
+INSTEAD OF INSERT
 AS
 BEGIN
-	DECLARE @nbJoursLocationReel INT
-	, @nbJoursLocationPrevue INT;
-
-	SET @nbJoursLocationPrevue = (SELECT DATEDIFF(DAY,dateDebutPrevue,dateFinPrevue) AS nbJoursPrevue FROM [location]);
-	SET @nbJoursLocationReel = (SELECT DATEDIFF(DAY,dateDebutReelle,dateFinReelle) AS nbJoursReel FROM [location]);
-
-	IF(@nbJoursLocationPrevue > @nbJoursLocationReel)
+	IF EXISTS(SELECT 1 FROM tarifLocation)
 	BEGIN
-		RETURN @nbJoursLocationPrevue;
+		RAISERROR('La table tarifLocation peut contenir un seul enregistrement, essayez de mettre à jour (update)?',11,1);
 	END
-
-	RETURN @nbJoursLocationReel;
+	INSERT INTO tarifLocation(fraisBase,fraisEssenceParLitreManquant,fraisBaseFixeEssence,fraisNettoyage,tauxTPS,tauxTVQ)
+		SELECT
+			fraisBase
+			,fraisEssenceParLitreManquant
+			,fraisBaseFixeEssence
+			,fraisNettoyage
+			,tauxTPS
+			,tauxTVQ
+		FROM inserted;
 END
-
-------------- Fonction pour obtenir la quantite d'essence restante en litre de la location ----------------------------
 
 GO
-CREATE OR ALTER FUNCTION FCT_ObtenirQuantiteEssenceRestante(@p_locationId INT)
-RETURNS INT
-AS
-BEGIN
-	RETURN 
-	(
-		SELECT DATEDIFF(DAY,dateDebutReelle,dateFinReelle) AS nbJours
-		FROM [location]
-		WHERE locationId = @p_locationId
-	);
-END
-
------------- Fonction pour calculer les frais de base par jour -----------------------------------------
-GO	
-CREATE OR ALTER FUNCTION FCT_CalculFraisBaseLocationParJour(@p_locationId INT)
-RETURNS DECIMAL(7,2)
-AS
-BEGIN
-
-	DECLARE @fraisBaseJours DECIMAL(5,2);
-
-	SET @fraisBaseJours = (SELECT * FROM FCT_ObtenirNbJoursLocations(@p_locationId)) * (SELECT fraisBase FROM tarifLocation);
-
-	RETURN @fraisBaseJours;
-END
-
------------------------- Fonction pour determiner si il y a des frais d'essence  ------------------------------------
-GO
-CREATE OR ALTER FUNCTION FCT_ADesFraisEssence(@p_locationId INT)
-RETURNS BIT
-AS
-BEGIN
-	DECLARE @quantiteEssenceRestante INT;
-	SET @quantiteEssenceRestante = (SELECT * FROM FCT_ObtenirQuantiteEssenceRestante(@p_locationId));
-	
-	IF(@quantiteEssenceRestante<40)
-	BEGIN
-		RETURN 1;
-	END
-	RETURN 0;
-END
-
------------------------- Fonction pour calculer les frais d'essence ---------------------------------------------
-GO
-CREATE OR ALTER FUNCTION FCT_CalculerFraisEssence(@p_locationId INT)
-RETURNS DECIMAL(7,2)
-AS
-BEGIN
-    IF((SELECT * FROM FCT_ADesFraisEssence(@p_locationId))=1)
-    BEGIN
-	    DECLARE @tarifFraisEssenceLitre DECIMAL(5,2)
-	    , @tarifFraisFixeEssence DECIMAL(5,2)
-	    , @fraisEssenceTotal DECIMAL(7,2);
-
-	    SET @tarifFraisEssenceLitre = (SELECT fraisEssenceParLitreManquant FROM tarifLocation);
-	    SET @tarifFraisFixeEssence = (SELECT fraisBaseFixeEssence FROM tarifLocation);
-	    SET @fraisEssenceTotal = 
-	    (
-		    (40 -(SELECT * FROM FCT_ObtenirQuantiteEssenceRestante(@p_locationId))) * 
-		    @tarifFraisEssenceLitre + 
-		    @tarifFraisFixeEssence
-	    );
-	    RETURN @fraisEssenceTotal;
-    END
-    RETURN 0.00;
-END
-
---------------- Fonction pour determiner si il y a des frais de nettoyage ------------------------------------
-GO
-CREATE OR ALTER FUNCTION FCT_ADesFraisDeNettoyage(@locationId INT)
-RETURNS BIT
-AS
-BEGIN
-
-	DECLARE @nbJoursLocation INT;
-	SET @nbJoursLocation = (SELECT * FROM FCT_ObtenirNbJoursLocation(@locationId));
-
-	IF(@nbJoursLocation>1)
-	BEGIN
-		RETURN 1;
-	END
-	RETURN 0;
-END
-GO
-
------------------- Fonction pour calculer les frais de nettoyage -------------------------------------------
-GO
-CREATE OR ALTER FUNCTION FCT_CalculFraisNettoyage(@p_locationId INT)
-RETURNS DECIMAL(7,2)
-AS
-BEGIN
-	IF((SELECT * FROM FCT_ADesFraisDeNettoyage(@p_locationId))=1)
-	BEGIN
-		DECLARE @tarifFraisNettoyage DECIMAL(5,2);
-		SET @tarifFraisNettoyage = (SELECT fraisNettoyage FROM tarifLocation);
-
-		RETURN @tarifFraisNettoyage;
-	END
-	RETURN 0.00;
-END
-
------===== Les 3 fonctions générales =====-----
--- FCT calcule du montant total hors taxes
-CREATE OR ALTER FUNCTION FCT_CalculeMontantTotalHorsTaxes(@p_locationId INT)
-RETURNS DECIMAL(7,2)
-AS
-BEGIN
-	DECLARE @fraisDeBase DECIMAL(7,2) = FCT_CalculFraisBaseLocationParJour(@p_locationId);
-	DECLARE @fraisDeNettoyage DECIMAL(7,2) = 0;
-	DECLARE @fraisEssence DECIMAL(7,2) = 0;
-
-	IF FCT_AdesFraisDeNettoyage(@p_locationId) = 1
-	BEGIN
-		SET @fraisDeNettoyage  = FCT_CalculeFraisDeNettoyage(@p_locationId);
-	END
-	IF FCT_ADesFraisEssence(@p_locationId) = 1
-	BEGIN
-		SET@fraisEssence = FCT_CalculeFraisEssence(@p_locationId);
-	END
-
-	DECLARE @montantTotalHorsTaxes DECIMAL(7,2) = @fraisDeBase + @fraisDeNettoyage + @fraisEssence;
-
-	RETURN @montantTotalHorsTaxes;
-END
-GO
-
---FCT calcule du montant total TPS
-CREATE OR ALTER FUNCTION FCT_CalculeMontantTPS(@p_locationId INT)
-RETURNS DECIMAL(7,2)
-AS
-BEGIN
-	DECLARE @MontantTotalHorsTaxes = (SELECT montantHorsTaxes  FROM facture
-									  WHERE locationId = @p_locationId);
-
-	DECLARE @tauxTPS DECIMAL(6,5) = (SELECT tauxTPS FROM tarifLocation);  
-
-	DECLARE @montantTPS DECIMAL(7,2) = @MontantTotalHorsTaxes * @tauxTPS;
-
-	RETURN @montantTPS;
-END
-GO
-
---FCT calcule du montant total TVQ
-CREATE OR ALTER FUNCTION FCT_CalculeMontantTVQ(@p_locationId INT)
-RETURNS DECIMAL(7,2)
-AS
-BEGIN
-	DECLARE @MontantTotalHorsTaxes = (SELECT montantHorsTaxes  FROM facture
-									  WHERE locationId = @p_locationId);
- 
-	DECLARE @tauxTVQ DECIMAL(6,5) = (SELECT tauxTVQ FROM tarifLocation); 
-
-
-	DECLARE @montantTVQ DECIMAL(7,2) = @MontantTotalHorsTaxes * @tauxTVQ;
-	
-
-	RETURN @montantTVQ;
-END
-GO
-
-
-
-
------=======  DAH TRIGGER  =======-----
--- trigger principal
 CREATE OR ALTER TRIGGER TR_Update_DateFinReelle_sur_Location
 ON [location]
 AFTER UPDATE
@@ -196,23 +29,20 @@ AS
 BEGIN
 	IF UPDATE (dateFinReelle)
 	BEGIN
-		DECLARE @cliendId UNIQUEIDENTIFIER = (SELECT cliendId FROM [location]
+		DECLARE @clientId UNIQUEIDENTIFIER = (SELECT clientId FROM [location]
 											  WHERE dateFinReelle = (SELECT dateFinReelle FROM inserted));
 
 		DECLARE @locationId INT = (SELECT locationId FROM [location]
-								   WHERE dateFinReelle = (SELECT dateFinReelle* FROM inserted));		
+								   WHERE dateFinReelle = (SELECT dateFinReelle FROM inserted));		
 		
 		DECLARE @nextfactureId INT = (SELECT MAX(factureId) + 1 FROM facture);
 
-		INSERT INTO facture (factureId)
-					VALUES (@nextfactureId);
+		DECLARE @montantHorsTaxes DECIMAL(7,2) = dbo.FCT_CalculeMontantTotalHorsTaxes(@nextfactureId);
+		DECLARE @montantTPS DECIMAL(7,2) = dbo.FCT_CalculeMontantTPS(@nextfactureId);
+		DECLARE @montantTVQ DECIMAL(7,2) = dbo.FCT_CalculeMontantTVQ(@nextfactureId);
 
-		DECLARE @montantHorsTaxes DECIMAL(7,2) = FCT_CalculeMontantHorsTaxes(@nextfactureId);
-		DECLARE @montantTPS DECIMAL(7,2) = FCT_CalculeMontantTPS(@nextfactureId);
-		DECLARE @montantTVQ DECIMAL(7,2) = FCT_CalculeMontantTVQ(@nextfactureId);
-
-		INSERT INTO facture (locationId, clientId, dateFacture, montantHorsTaxes, montantTaxesTVQ, montantTaxesTPS)
-			         VALUES (@locationId, @cliendId, GETDATE(), @montantHorsTaxes, @montantTVQ, @montantTPS);
+		INSERT INTO facture (factureId,locationId, clientId, dateFacture, montantHorsTaxes, montantTaxesTVQ, montantTaxesTPS)
+			         VALUES (@nextfactureId,@locationId, @clientId, GETDATE(), @montantHorsTaxes, @montantTVQ, @montantTPS);
           
 	END;
 END;
@@ -224,16 +54,16 @@ ON facture
 AFTER INSERT
 AS 
 BEGIN
-    DECLARE @nextFactureLigneId INT = (SELECT MAX(factureLigneId) + 1 FROM factureLigfne);
+    DECLARE @nextFactureLigneId INT = (SELECT MAX(factureLigneId) + 1 FROM factureLigne);
     DECLARE @factureId INT = (SELECT factureId FROM inserted);
     DECLARE @fraisbase DECIMAL(7,2) = (SELECT fraisBase FROM tarifLocation);
     DECLARE @locationId INT = (SELECT locationId FROM inserted);
-    DECLARE @nbJoursLocation INT = FCT_ObtenirNbJoursLocation(@locationId);
-    DECLARE @description TEXT = "Frais de base et nombre de jour de location."
+    DECLARE @nbJoursLocation INT = dbo.FCT_ObtenirNbJoursLocation(@locationId);
+    DECLARE @description VARCHAR(80) = 'Frais de base et nombre de jour de location.'
     DECLARE @tauxTVQ DECIMAL(3,2) = (SELECT tauxTVQ FROM tarifLocation);
     DECLARE @tauxTPS DECIMAL(3,2) = (SELECT tauxTPS FROM tarifLocation);
 
-    INSERT INTO factureLigne (factureLigneId, facturId, prixUnitaire, quantite, [description], tauxTaxesTVQ, tauxTaxesTPS)
+    INSERT INTO factureLigne (factureLigneId, factureId, prixUnitaire, quantite, [description], tauxTaxesTVQ, tauxTaxesTPS)
                 VALUES (@nextFactureLigneId, @factureId, @fraisbase, @nbJoursLocation, @description, @tauxTVQ, @tauxTPS);
 END;
 GO
@@ -244,17 +74,18 @@ ON facture
 AFTER INSERT
 AS
 BEGIN
-    DECLARE @nextFactureLigneId INT = (SELECT MAX(factureLigneId) + 1 FROM factureLigfne);
+    DECLARE @nextFactureLigneId INT = (SELECT MAX(factureLigneId) + 1 FROM factureLigne);
     DECLARE @factureId INT = (SELECT factureId FROM inserted);
     
     DECLARE @locationId INT = (SELECT locationId FROM inserted);
-    DECLARE @fraisNettoyage DECIMAL(7,2) = FCT_CalculeFraisNettoyage;
+    DECLARE @fraisNettoyage DECIMAL(7,2) = dbo.FCT_CalculFraisNettoyage(@locationId);
 
-    DECLARE @description TEXT = "Frais de nettoyage."
+	DECLARE @nbJoursLocation INT = dbo.FCT_ObtenirNbJoursLocation(@locationId)
+    DECLARE @description VARCHAR(80) = 'Frais de nettoyage.'
     DECLARE @tauxTVQ DECIMAL(3,2) = (SELECT tauxTVQ FROM tarifLocation);
     DECLARE @tauxTPS DECIMAL(3,2) = (SELECT tauxTPS FROM tarifLocation);
 
-    INSERT INTO factureLigne (factureLigneId, facturId, prixUnitaire, quantite, [description], tauxTaxesTVQ, tauxTaxesTPS)
+    INSERT INTO factureLigne (factureLigneId, factureId, prixUnitaire, quantite, [description], tauxTaxesTVQ, tauxTaxesTPS)
                 VALUES (@nextFactureLigneId, @factureId, @fraisNettoyage, @nbJoursLocation, @description, @tauxTVQ, @tauxTPS);
 END
 GO
@@ -265,17 +96,17 @@ ON facture
 AFTER INSERT
 AS 
 BEGIN
-    DECLARE @nextFactureLigneId INT = (SELECT MAX(factureLigneId) + 1 FROM factureLigfne);
+    DECLARE @nextFactureLigneId INT = (SELECT MAX(factureLigneId) + 1 FROM factureLigne);
     DECLARE @factureId INT = (SELECT factureId FROM inserted);
 
     DECLARE @locationId INT = (SELECT locationId FROM inserted);
-    DECLARE @fraisEssence DECIMAL(7,2) = FCT_CalculerFraisEssence(@p_locationId);
+    DECLARE @fraisEssence DECIMAL(7,2) = dbo.FCT_CalculeFraisEssence(@locationId);
 
-    DECLARE @description TEXT = "Frais d'essence."
+	DECLARE @nbJoursLocation INT = dbo.FCT_ObtenirNbJoursLocation(@locationId)
+    DECLARE @description VARCHAR(80) = 'Frais d''essence.'
     DECLARE @tauxTVQ DECIMAL(3,2) = (SELECT tauxTVQ FROM tarifLocation);
     DECLARE @tauxTPS DECIMAL(3,2) = (SELECT tauxTPS FROM tarifLocation);
 
-    INSERT INTO factureLigne (factureLigneId, facturId, prixUnitaire, quantite, [description], tauxTaxesTVQ, tauxTaxesTPS)
+    INSERT INTO factureLigne (factureLigneId, factureId, prixUnitaire, quantite, [description], tauxTaxesTVQ, tauxTaxesTPS)
                 VALUES (@nextFactureLigneId, @factureId, @fraisEssence, @nbJoursLocation, @description, @tauxTVQ, @tauxTPS);
 END
-GO
